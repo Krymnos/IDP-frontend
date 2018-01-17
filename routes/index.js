7b826575ec4c92d1a8cc8ba43072d8d059e9dac6
Darshan Hingu <hingu.darshan@gmail.com>
2018-01-17 13:14:20 +0100

    Click Event of Nodes from Visualisation + Changed Design

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
-
+const url = "http://122.129.79.67:5000/v1";
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
     let list = {};
     let nodes = [];
     let edges = [];
-    axios.get('http://122.129.79.67:5000/v1/nodes')
+    axios.get(url+'/nodes')
         .then(response => {
         list = response.data;
         list.forEach(x => {
@@ -22,7 +22,21 @@ router.get('/', function(req, res, next) {
                 });
             }
         });
-    res.render('index', { title: 'Express',nodes: JSON.stringify(nodes), edges:JSON.stringify(edges) });
+
+        let datapoints = {};
+        let data = [];
+        axios.get(url+'/provenance/5a5a8538000253abcd12').then(response => {
+            var id = response.data.id.substr(response.data.id.length-6);
+            var node_name = nodes.find(node_name => node_name.id === id);
+            datapoints = response.data.inputDatapoints;
+            data.push({id:node_name.id, name: node_name.label, app: response.data.context.appName,classname: response.data.context.className,meter: response.data.context.meterId, receive: response.data.context.receiveTime})
+            console.log(data);
+
+            res.render('index', { title: 'Express',nodes: JSON.stringify(nodes), edges:JSON.stringify(edges), dp: data });
+        }).catch(error=>{
+            console.log(error);
+        });
+
     }).catch(error => {
             console.log(error);
     });
